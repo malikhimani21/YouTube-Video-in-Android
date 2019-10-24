ddb37b685b55b150485e5b09e9a61413af48987c
malik himani <sunnyhimani21@gmail.com>
2019-10-24 22:39:45 +0530

    Integrate youtube videos in android app

---
+++ app/src/main/java/com/example/malikhimani/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.malikhimani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    private static final String YouTubeAPI_KEY = "your api key here";
+    YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youTube);
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("_tOAQ0lWEVg");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        youTubePlayerView.initialize(YouTubeAPI_KEY, onInitializedListener);
+    }
+}
